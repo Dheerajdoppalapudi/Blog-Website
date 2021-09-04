80ab7576aef582ad2a0e55a0b64c8e674f4e1b96
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2021-09-04 17:53:00 +0530

    added search route

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('search/', views.search_name, name='search')
 ]
