891a998cdac65cdc1c535c97f1b6769f32639cbe
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2021-09-05 11:49:16 +0530

    added sql querying

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from users.models import User
 
 @login_required
 def home(request):
@@ -16,6 +17,16 @@ def about(request):
 def search_name(request):
     if request.method == "POST":
         form_data = request.POST.get('title')
+        try:
+            current_user = User.objects.get(username=form_data)
+            post = Post.objects.filter(author=current_user)
+            context = {
+                "user_posts": post,
+                "queried_user": form_data
+            }
+            return render(request, 'blog/search.html', context)
+        except User.DoesNotExist:
+            pass
     context = {
         "queried_user": form_data,
         "message": "nothing matched your search result",
